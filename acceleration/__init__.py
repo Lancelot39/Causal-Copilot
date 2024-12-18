c5c7751961ab6bcd327130dcfdc99a62f77d4caf
Rubia Shen <rubia.shen@Rubias-MacBook-Pro.local>
2024-12-17 19:22:10 -0800

    Integrate Software Acceleration on GES (FGES, XGES)

---
+++ acceleration/__init__.py
@@ -0,0 +1,4 @@
+from .accelerated_lingam.directlingam import DirectLiNGAM as AcceleratedDirectLiNGAM
+from .accelerated_ges.fges.fges import FGES
+
+__all__ = ['AcceleratedDirectLiNGAM', 'FGES']
--- algorithm/wrappers/__init__.py
+++ algorithm/wrappers/__init__.py
@@ -2,8 +2,10 @@ from .pc import PC
 from .fci import FCI
 from .cdnod import CDNOD
 from .ges import GES
+from .fges import FGES
+from .x_ges import XGES
 from .direct_lingam import DirectLiNGAM
 from .ica_lingam import ICALiNGAM
 from .notears import NOTEARS
 
-__all__ = ['PC', 'FCI', 'CDNOD', 'GES', 'DirectLiNGAM', 'ICALiNGAM', 'NOTEARS']
+__all__ = ['PC', 'FCI', 'CDNOD', 'GES', 'FGES', 'XGES', 'DirectLiNGAM', 'ICALiNGAM', 'NOTEARS']
